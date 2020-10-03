4d6a791494faeb6cd4b4108583148786cfe88ec9
Suhun Han <ssut@ssut.me>
2020-10-04 03:59:34 +0900

    feat(payload): read and extract from the payload

--- payload.go
+++ payload.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"bytes"
+	"compress/bzip2"
+	"crypto/sha256"
 	"encoding/binary"
+	"encoding/hex"
+	"errors"
 	"fmt"
+	"io"
 	"os"
 
 	humanize "github.com/dustin/go-humanize"
 	"github.com/golang/protobuf/proto"
+	xz "github.com/spencercw/go-xz"
 	"github.com/ssut/payload-dumper-go/chromeos_update_engine"
 )
 
@@ -21,10 +28,12 @@ type Payload struct {
 
 	metadataSize int64
 	dataOffset   int64
+	initialized  bool
 }
 
 const payloadHeaderMagic = "CrAU"
 const brilloMajorPayloadVersion = 2
+const blockSize = 4096
 
 type payloadHeader struct {
 	Version              uint64
@@ -163,6 +172,123 @@ func (p *Payload) Init() error {
 		} else {
 			fmt.Printf("\n")
 		}
+	}
+	p.initialized = true
+
+	return nil
+}
+
+func (p *Payload) readDataBlob(offset int64, length int64) ([]byte, error) {
+	buf := make([]byte, length)
+	n, err := p.file.ReadAt(buf, p.dataOffset+offset)
+	if err != nil {
+		return nil, err
+	}
+	if int64(n) != length {
+		return nil, fmt.Errorf("Read length mismatch: %d != %d", n, length)
+	}
+
+	return buf, nil
+}
+
+func (p *Payload) Extract(partition *chromeos_update_engine.PartitionUpdate, out *os.File) error {
+	name := partition.GetPartitionName()
+	for _, operation := range partition.Operations {
+		if len(operation.DstExtents) == 0 {
+			return fmt.Errorf("Invalid operation.DstExtents for the partition %s", name)
+		}
+		e := operation.DstExtents[0]
+		data, err := p.readDataBlob(int64(operation.GetDataOffset()), int64(operation.GetDataLength()))
+		if err != nil {
+			return err
+		}
+		_, err = out.Seek(int64(e.GetStartBlock())*blockSize, 0)
+		if err != nil {
+			return err
+		}
+		expectedUncompressedBlockSize := int64(e.GetNumBlocks() * blockSize)
+		// fmt.Println(e.GetNumBlocks(), blockSize)
+
+		buf := bytes.NewBuffer(data)
+		bufSha := sha256.New()
+		bufSha.Write(data)
+		hash := hex.EncodeToString(bufSha.Sum(nil))
+		expectedHash := hex.EncodeToString(operation.GetDataSha256Hash())
+		if hash != expectedHash {
+			return fmt.Errorf("Verify failed (Checksum mismatch): %s (%s != %s)", name, hash, expectedHash)
+		}
+
+		switch operation.GetType() {
+		case chromeos_update_engine.InstallOperation_REPLACE:
+			n, err := out.Write(data)
+			if err != nil {
+				return err
+			}
+			if int64(n) != expectedUncompressedBlockSize {
+				return fmt.Errorf("Verify failed (Unexpected bytes written): %s (%d != %d)", name, n, expectedUncompressedBlockSize)
+			}
+			break
+
+		case chromeos_update_engine.InstallOperation_REPLACE_XZ:
+			reader := xz.NewDecompressionReader(buf)
+			if err != nil {
+				return err
+			}
+
+			n, err := io.Copy(out, &reader)
+			if err != nil {
+				return err
+			}
+			if n != expectedUncompressedBlockSize {
+				return fmt.Errorf("Verify failed (Unexpected bytes written): %s (%d != %d)", name, n, expectedUncompressedBlockSize)
+			}
+
+			break
+
+		case chromeos_update_engine.InstallOperation_REPLACE_BZ:
+			reader := bzip2.NewReader(buf)
+			n, err := io.Copy(out, reader)
+			if err != nil {
+				return err
+			}
+			if n != expectedUncompressedBlockSize {
+				return fmt.Errorf("Verify failed (Unexpected bytes written): %s (%d != %d)", name, n, expectedUncompressedBlockSize)
+			}
+			break
+
+		default:
+			return fmt.Errorf("Unhandled operation type: %s", operation.GetType().String())
+		}
+
+	}
+
+	return nil
+}
+
+func (p *Payload) ExtractAll(targetDirectory string) error {
+	if !p.initialized {
+		return errors.New("Payload has not been initialized")
+	}
+
+	total := len(p.deltaArchiveManifest.Partitions)
+	for i, partition := range p.deltaArchiveManifest.Partitions {
+		info := partition.GetNewPartitionInfo()
+		name := fmt.Sprintf("%s.img", partition.GetPartitionName())
+
+		fmt.Printf("[%02d/%02d] Extracting: %s\n", i+1, total, fmt.Sprintf("%s (%s)", name, humanize.Bytes(*info.Size)))
+
+		filepath := fmt.Sprintf("%s/%s", targetDirectory, name)
+		file, err := os.OpenFile(filepath, os.O_TRUNC|os.O_CREATE|os.O_WRONLY, 0755)
+		if err != nil {
+			file.Close()
+			return err
+		}
+		if err := p.Extract(partition, file); err != nil {
+			file.Close()
+			return err
+		}
+		file.Close()
+
 	}
 
 	return nil
